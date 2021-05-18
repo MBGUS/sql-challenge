e7ad7848e3d4c6d441acdcd8518782f141211828
Gustavo M <gusmaldonadob@gmail.com>
2021-05-17 19:01:41 -0500

    Adding answer 4 & 5

--- EmployeeSQL/SQLCode.sql
+++ EmployeeSQL/SQLCode.sql
@@ -24,3 +24,25 @@ SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employe
 FROM dept_manager
 INNER JOIN departments ON dept_manager.dept_no = departments.dept_no
 INNER JOIN employees ON employees.emp_no = dept_manager.emp_no;
+
+-- 4. List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
+-- As the information is from many tables, we use JOIN to get the information back.
+
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no;
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- Using "WHERE" to filter and specific information, in this case, first name= "Hercules".
+-- Using "LIKE" & "%" to find results which last name begins with "B".
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE
+first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+
+
