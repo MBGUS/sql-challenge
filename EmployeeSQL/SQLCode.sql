35b99d884099da9f61b54a534b05b96271a1481b
Gustavo M <gusmaldonadob@gmail.com>
2021-05-17 19:10:34 -0500

    Adding code for queries 6 & 7

--- EmployeeSQL/SQLCode.sql
+++ EmployeeSQL/SQLCode.sql
@@ -1,8 +1,7 @@
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
-
 -- With SELECT function we declare the main variables for our table.
 -- TIP: Table & . & Column, example: employees.emp_no
--- Requiered a Join to get the "salary" data, using the PK emp_no. 
+-- Requiered a Join to get the "salary" data, using the PK emp_no.  Check ERD Diagram.
 
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
@@ -18,7 +17,7 @@ WHERE hire_date >= '1986-01-01' and hire_date <= '1986-12-31';
 
 -- 3.List the manager of each department with the following information: 
 -- department number, department name, the manager's employee number, last name, first name.
--- As the information is from many tables, we use JOIN to get the information back.
+-- As the information is from many tables, we use JOIN to get the information back. Check ERD Diagram.
 
 SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
 FROM dept_manager
@@ -27,7 +26,7 @@ INNER JOIN employees ON employees.emp_no = dept_manager.emp_no;
 
 -- 4. List the department of each employee with the following information: 
 -- employee number, last name, first name, and department name.
--- As the information is from many tables, we use JOIN to get the information back.
+-- As the information is from many tables, we use JOIN to get the information back. Check ERD Diagram.
 
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
@@ -44,5 +43,27 @@ WHERE
 first_name = 'Hercules'
 AND last_name LIKE 'B%';
 
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- As the information is from many tables, we use JOIN to get the information back. Check ERD Diagram.
+-- Using "WHERE" to filter and specific information, in this case, employees in Sales Department. 
+
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
+-- 7. List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
+-- As the information is from many tables, we use JOIN to get the information back. Check ERD Diagram.
+-- In this case use "OR", which is an inclusive parameter to find people in Sales or Development.
+
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE
+departments.dept_name = 'Sales'
+OR departments.dept_name = 'Development';
 
 
